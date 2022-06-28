20b18d85f898838d3fc10c8e225800fe7ce2ab28
Sam Penny <33956017+sam1penny@users.noreply.github.com>
2022-06-28 14:28:46 +0100

    added compatibility with more versions of anki for search_cards_based_on_length

--- src/search_cards_based_on_length/__init__.py
+++ src/search_cards_based_on_length/__init__.py
@@ -2,6 +2,7 @@ import aqt
 from aqt import gui_hooks
 from aqt import mw
 from typing import List
+import anki
 
 
 def createSearchString(stype: str, n: int) -> str:
@@ -18,10 +19,8 @@ def createSearchString(stype: str, n: int) -> str:
             s += ' OR '
     return s
 
-def customSearch(context: aqt.browser.SearchContext) -> None:
-    if context.card_ids is not None:
-        return
 
+def modifySearchStringIfNecessary(context: aqt.browser.SearchContext) -> None:
     if context.search.startswith("min:"):
         n = int(context.search.split("min:")[1].split(" ")[0])
         context.search = createSearchString("min", n)
@@ -33,6 +32,21 @@ def customSearch(context: aqt.browser.SearchContext) -> None:
     elif context.search.startswith("max:"):
         n = context.search.split("max:")[1].split(" ")[0]
         context.search = createSearchString("max", n)
+
+
+if anki.version >= "2.1.45":
+    def customSearch(context: aqt.browser.SearchContext) -> None:
+        if context.ids is not None:
+            return
+
+        modifySearchStringIfNecessary(context)
+
+else:
+    def customSearch(context: aqt.browser.SearchContext) -> None:
+        if context.card_ids is not None:
+            return
+
+        modifySearchStringIfNecessary(context)
 
 
 config = mw.addonManager.getConfig(__name__)
